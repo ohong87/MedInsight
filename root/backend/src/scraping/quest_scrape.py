7ec4d2d64f30ab5bebbe14ef43b45226ca43175b
Oscar Hong <othong@usc.edu>
2024-02-13 12:27:02 -0800

    updated pdfs

--- root/backend/src/scraping/quest_scrape.py
+++ root/backend/src/scraping/quest_scrape.py
@@ -1,17 +1,21 @@
 from PyPDF2 import PdfReader
 
+all_tests = {} # {test : range}
+data = {} # output to be returned as json
+
 # Open the PDF file
-reader = PdfReader("/Users/oscarh/Projects/MedInsight/root/backend/pdfs/quest1.pdf") # temp path for Oscar's local computer
-page = reader.pages[0]  # Change 0 to the page number you're interested in, zero-indexed
+reader = PdfReader("/Users/oscarh/Projects/MedInsight/root/backend/pdfs/quest2.pdf") # temp path for Oscar's local computer
+page = reader.pages[3]  # Change 0 to the page number you're interested in, zero-indexed
 
 parts = []
 
 def visitor_body(text, cm, tm, fontDict, fontSize):
     y = tm[5]
-    if 33 <= y <= 544:
-        parts.append(text)
+    parts.append(text)
 
 page.extract_text(visitor_text=visitor_body)
 text_body = "".join(parts)
 
 print(text_body)
+
+# TODO: extract relevant data and store in data dictionary and send json to db
