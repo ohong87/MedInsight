ac0e8a3794bb0528e4d420d63fdce970818d8917
kerry <kmenglin@usc.edu>
2024-02-22 17:07:24 -0800

    cleaned up boiler plate code and added section for dashboard

---
+++ root/frontend/src/pages/dashboard.tsx
@@ -0,0 +1,29 @@
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+
+export const Dashboard: React.FC = () => {
+  return (
+    <Container
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#F9F9F9"
+      }}
+    >
+        <p>meewfoefsdfasfjsadkfhasdfhasdjkfhasdjfhajdfh</p>
+      
+      
+      <Box
+        display="flex"
+        gap="36px"
+        justifyContent="center"
+        flexDirection="column"
+        style={{
+          backgroundColor: "#2C4F6F"}}
+      >
+      </Box>
+    </Container>
+  );
+};
--- root/frontend/src/pages/login.tsx
+++ root/frontend/src/pages/login.tsx
@@ -4,12 +4,9 @@ import { useEffect, useRef } from "react";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 
-// import Typography from "@mui/material/Typography";
-// import { ThemedTitleV2 } from "@refinedev/mui";
-
 import { CredentialResponse } from "../interfaces/google";
 
-console.log(process.env.GOOGLE_CLIENT_ID);
+// console.log(process.env.GOOGLE_CLIENT_ID);
 
 export const Login: React.FC = () => {
   const { mutate: login } = useLogin<CredentialResponse>();
